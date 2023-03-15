4d62afb41e6329d98e73c08c331f46f959341b93
Negi <himanshu.c.negi@capgemini.com>
2023-03-15 21:53:59 +0530

    Adding Facade Design

--- src/main/java/com/cg/client/Main.java
+++ src/main/java/com/cg/client/Main.java
@@ -1,10 +1,5 @@
 package com.cg.client;
 
-import com.cg.actionhandler.ActionHandler;
-import com.cg.exceptions.AccountNotFoundException;
-import com.cg.exceptions.InsufficientBalanceException;
-import com.cg.xmlparser.Parser;
-
 /**
  * Class to test the action handler. Sample class that resembles the client code
  * that might generate errors.
@@ -13,25 +8,8 @@ import com.cg.xmlparser.Parser;
  *
  */
 public class Main {
-	static ActionHandler handler = new ActionHandler();
-
-	public static void WithdrawAmount() {
-		handler.handle(new AccountNotFoundException());
-	}
-
-	public static void DepositAmount() {
-		handler.handle(new AccountNotFoundException());
-	}
-
-	public static void FundTransfer() {
-		handler.handle(new InsufficientBalanceException());
-	}
 
 	public static void main(String[] args) {
-		Parser.readXML(); // must be callable once
-
-		WithdrawAmount();
-//		FundTransger();
-//		DepositAmount();
+		StartBankApp.start();
 	}
 }
